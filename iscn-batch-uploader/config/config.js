eb0810b56a8fd028b4d610f3967c926c46c98a7b
Sze Wong <swong@zerionsoftware.com>
2023-02-26 12:36:58 -0800

    Added settings for Craft your world

--- iscn-batch-uploader/config/config.js
+++ iscn-batch-uploader/config/config.js
@@ -1,7 +1,9 @@
 const config = {};
 
-config.ISCN_RPC_URL = 'https://mainnet-node.like.co/rpc/';
-config.COSMOS_MNEMONIC = '';
-config.COSMOS_DENOM = 'nanolike';
+//config.ISCN_RPC_URL = 'https://mainnet-node.like.co/rpc/';
+//config.COSMOS_DENOM = 'nanolike';
+config.ISCN_RPC_URL = 'https://node.testnet.like.co/rpc/';
+config.COSMOS_MNEMONIC = 'there marine park pumpkin minor age way patrol gaze dove rug latin jump poem lake push cement thing name practice prison arrow trip capital';
+config.COSMOS_DENOM = 'nanoekil';
 
 module.exports = config;
